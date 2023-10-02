8701a60bd60dcf7513ca4a0165297b1f9f74636d
B19KosikA <B19KosikA@204-6>
2023-10-02 12:49:45 +0200

    Add project files.

---
+++ H20231002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H20231002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Alex";
+            Console.WriteLine($"{name}");
+        }
+    }
+}
